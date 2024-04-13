8669959e8418d42563960381432614afd12bcbfa
Pranav Ramesh <pranav.ramesh1@gmail.com>
2024-04-13 15:34:00 -0400

    [PR] Added VideoStitcher

--- whisper_func.py
+++ whisper_func.py
@@ -1,22 +1,24 @@
 import whisper
-from pyannote.audio import Pipeline
-from utils import diarize_text
-import torch
-from collections import namedtuple
-import time
-from moviepy.editor import VideoFileClip
+
+# from whispercpp import Whisper
 
 
 class VideoToTextModel:
 
     def __init__(self):
         self.model = whisper.load_model("small.en")
+        # self.model = Whisper.from_pretrained("small.en")
 
     def raw_transcribe(self, video_path):
         return self.model.transcribe(video_path)
 
     def sentence_transcribe(self, video_path):
         transcription = self.raw_transcribe(video_path)["segments"]
+        # self.raw_transcribe(video_path)
+        # transcription = self.model.transcribe_from_file(video_path)
+        # transcription = self.model.ctx
+
+        # print("transcription:", type(transcription))
 
         intervals = [[]]
         for idx, segment in enumerate(transcription):
