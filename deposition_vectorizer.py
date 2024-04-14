a1d71a99a28e10367ff458bc109ccc3ec6216649
Christopher Arraya <arrayacj@gmail.com>
2024-04-14 15:18:37 -0400

    don't revectorize things in the vectordb

--- deposition_vectorizer.py
+++ deposition_vectorizer.py
@@ -8,9 +8,6 @@ from llama_index.core.node_parser import NodeParser
 from whisper_func import VideoToTextModel
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core import SimpleDirectoryReader
-import asyncio
-
-
 
 class DepositionVectorizer:
     def __init__(self, pinecone_index_name, pinecone_api_key, togetherai_api_key):
@@ -21,23 +18,29 @@ class DepositionVectorizer:
         )
         self.vector_store = PineconeVectorStore(pinecone_index=self.index)
         
-    async def vectorize_and_upsert_pdf(self, pdf_path):
+    def vectorize_and_upsert_pdf(self, pdf_path):
+        res = self.query_text("a", top_k=1, filter={"file_path": {"$eq": pdf_path}})
+        if (len(res["matches"]) > 0):
+          return
         documents = SimpleDirectoryReader(input_files=[pdf_path]).load_data()
         node_parser = SentenceSplitter(chunk_size=512, chunk_overlap=20, include_metadata=True)
         nodes = node_parser.get_nodes_from_documents(documents)
-        vectors = [{"id": node.node_id, "values": await self.vectorize_text(node.get_content()), "metadata": {**node.metadata, "text": node.get_content()}} for node in nodes]
+        vectors = [{"id": node.node_id, "values": self.vectorize_text(node.get_content()), "metadata": {**node.metadata, "text": node.get_content()}} for node in nodes]
         self.index.upsert(vectors=vectors)
         
-    async def vectorize_and_upsert_video(self, video_path):
+    def vectorize_and_upsert_video(self, video_path):
+        res = self.query_text("a", top_k=1, filter={"video_path": {"$eq": video_path}})
+        if (len(res["matches"]) > 0):
+          return
         video_to_text = VideoToTextModel()
         text = video_to_text.sentence_transcribe(video_path)
-        vectors = [{"id": str(i), "values": await self.vectorize_text(seg["text"]), "metadata": {"start": seg["start"], "end": seg["end"], "video_path": video_path, "text": seg["text"]}} for i, seg in enumerate(text)]
+        vectors = [{"id": str(i), "values": self.vectorize_text(seg["text"]), "metadata": {"start": seg["start"], "end": seg["end"], "video_path": video_path, "text": seg["text"]}} for i, seg in enumerate(text)]
         self.index.upsert(vectors=vectors)
       
-    async def vectorize_text(self, text):
-        return await self.embedder.aget_text_embedding(text)
+    def vectorize_text(self, text):
+        return self.embedder.get_text_embedding(text)
     
-    async def query_text(self, text, top_k=3, filter=None):
-        query_vector = await self.vectorize_text(text)
+    def query_text(self, text, top_k=3, filter=None):
+        query_vector = self.vectorize_text(text)
         return self.index.query(vector=query_vector, top_k=top_k, include_values=False, include_metadata=True, filter=filter)
 
